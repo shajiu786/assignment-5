a0c521b620fa8d6636086b674ff492336b99ec0f
shajiu786 <shajiu786@gmail.com>
2025-02-26 21:41:28 +0530

    Add files via upload

---
+++ a5t1.py
@@ -0,0 +1,7 @@
+dict={'shaji':85,'jash':95}
+a=input("enter the students name:")
+if a in dict:
+ print(a,"'s","marks:",dict[a])
+
+else :
+    print("student not found")
---
+++ a5t2.py
@@ -0,0 +1,5 @@
+num=[1,2,3,4,5,6,7,8,9,10]
+print("original list :",num)
+a=num[0:4]
+print("extracted first 5 elements:",a)
+print("reversed extracted numbers",a[::-1])
